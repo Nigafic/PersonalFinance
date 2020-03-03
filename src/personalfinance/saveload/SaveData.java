63753e0d22a35c74600ceb526bd158c9cedbd715
Nigafic <1302035246ghost@gmail.com>
2020-03-03 12:22:39 +0300

    Реализация мезанизма сохранения данных Механизм

--- src/personalfinance/saveload/SaveData.java
+++ src/personalfinance/saveload/SaveData.java
@@ -25,6 +25,26 @@ public class SaveData {
         return instance;
     }
 
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void setCurrencies(List<Currency> currencies) {
+        this.currencies = currencies;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public void setTransfers(List<Transfer> transfers) {
+        this.transfers = transfers;
+    }
+
     public List<Account> getAccounts() {
         return accounts;
     }
